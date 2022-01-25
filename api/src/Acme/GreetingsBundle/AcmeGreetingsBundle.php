3e392cd7521e6ef9f762c7da4c83117ba6e5609e
Richard BAYET <richard.bayet@smile.fr>
2022-01-25 16:35:16 +0100

    ESPP-173 Automatic Doctrine Mapping for Bundles (fixManagersAutoMappings)

---
+++ api/src/Acme/GreetingsBundle/AcmeGreetingsBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @category  Acme
+ * @package   Acme\GreetingsBundle
+ * @author    Richard Bayet <richard.bayet@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+namespace Acme\GreetingsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeGreetingsBundle extends Bundle
+{
+
+}
