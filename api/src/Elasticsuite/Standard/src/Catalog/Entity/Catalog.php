cb7bf5062ed14acad1519bc14eb96c541db9cad9
Romain Ruaud <romain.ruaud@smile.fr>
2022-02-11 15:22:26 +0100

    Adding Website and Catalog entities.

--- api/src/Elasticsuite/Standard/src/Catalog/Entity/Catalog.php
+++ api/src/Elasticsuite/Standard/src/Catalog/Entity/Catalog.php
@@ -17,14 +17,12 @@ declare(strict_types=1);
 namespace Elasticsuite\Catalog\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * This is a dummy entity. Remove it!
- */
-#[ApiResource(mercure: true)]
-#[ORM\Entity]
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'delete'],
+)]
+
 class Catalog
 {
     /**
@@ -41,9 +39,62 @@ class Catalog
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $name = '';
+    private $code;
+
+    private $locale;
+
+    private $website;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?Website
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(?Website $website): self
+    {
+        $this->website = $website;
+
+        return $this;
     }
 }
---
+++ api/src/Elasticsuite/Standard/src/Catalog/Repository/CatalogRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Elasticsuite\Catalog\Repository;
+
+use Elasticsuite\Catalog\Entity\Catalog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Catalog|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Catalog|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Catalog[]    findAll()
+ * @method Catalog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CatalogRepository extends ServiceEntityRepository implements WebsiteRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Catalog::class);
+    }
+
+    // /**
+    //  * @return Catalog[] Returns an array of Catalog objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Catalog
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
