535e947ec36ffab5943c6a1296cd25d7ed166c71
Romain Ruaud <romain.ruaud@smile.fr>
2022-02-11 15:22:26 +0100

    Adding Website and Catalog entities.

--- api/src/Elasticsuite/Standard/src/Catalog/Entity/Catalog.php
+++ api/src/Elasticsuite/Standard/src/Catalog/Entity/Catalog.php
@@ -3,33 +3,74 @@
 namespace Elasticsuite\Catalog\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * This is a dummy entity. Remove it!
- */
-#[ApiResource(mercure: true)]
-#[ORM\Entity]
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get', 'delete'],
+)]
+
 class Catalog
 {
-    /**
-     * The entity ID
-     */
-    #[ORM\Id]
-    #[ORM\Column(type: 'integer')]
-    #[ORM\GeneratedValue]
-    private ?int $id = null;
+    private $id;
 
-    /**
-     * A nice person
-     */
-    #[ORM\Column]
-    #[Assert\NotBlank]
-    public string $name = '';
+    private $name;
+
+    private $code;
+
+    private $locale;
+
+    private $website;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?Website
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(?Website $website): self
+    {
+        $this->website = $website;
+
+        return $this;
     }
 }
---
+++ api/src/Elasticsuite/Standard/src/Catalog/Repository/CatalogRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Elasticsuite\Catalog\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
+
+interface CatalogRepositoryInterface extends ServiceEntityRepositoryInterface
+{
+
+}
