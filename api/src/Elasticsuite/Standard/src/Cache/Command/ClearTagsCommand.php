f4d19afa3d6a9bcefdc325fb9925b65c2845ac11
Richard BAYET <richard.bayet@smile.fr>
2022-02-24 19:17:59 +0100

    ESPP-196 Adding HTTP cache clearing to CLI command

--- api/src/Elasticsuite/Standard/src/Cache/Command/ClearTagsCommand.php
+++ api/src/Elasticsuite/Standard/src/Cache/Command/ClearTagsCommand.php
@@ -15,18 +15,20 @@
 
 namespace Elasticsuite\Cache\Command;
 
+use ApiPlatform\Core\HttpCache\PurgerInterface as HttpPurgerInterface;
 use Elasticsuite\Cache\Service\CacheManagerInterface;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ClearTagsCommand extends Command
 {
     protected static $defaultName = 'elasticsuite:cache:clear-tags';
 
-    public function __construct(private CacheManagerInterface $cache, string $name = null)
+    public function __construct(private CacheManagerInterface $cache, private HttpPurgerInterface $httpPurger, string $name = null)
     {
         parent::__construct($name);
     }
@@ -40,11 +42,12 @@ class ClearTagsCommand extends Command
         $this
             ->setDefinition([
                 new InputArgument('tags', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'The cache tags to invalidate cache for'),
+                new InputOption('http', null, InputOption::VALUE_NONE, 'Also try to invalidate the HTTP cache associated with the cache tags'),
             ])
             ->setHelp(<<<'EOF'
 The <info>%command.name%</info> clears cache objects identified by their cache tag(s) in elasticsuite specific cache pool.
 
-    %command.full_name% <tags>...
+    %command.full_name% [--http] <tags>...
 EOF
             )
         ;
@@ -59,6 +62,10 @@ EOF
 
         try {
             $this->cache->clearTags($tags);
+            if ($input->hasOption('http')) {
+                $this->httpPurger->purge($tags);
+            }
+
         } catch (InvalidArgumentException $e) {
             $output->writeln($e->getMessage());
 
