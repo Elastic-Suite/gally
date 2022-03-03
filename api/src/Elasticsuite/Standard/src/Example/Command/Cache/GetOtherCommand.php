c0f18564445e4866f0fcd09749533a951c6b0a05
Romain Ruaud <romain.ruaud@smile.fr>
2022-03-03 18:51:42 +0100

    ESPP-74 Applying coding standards to existing files.

--- api/src/Elasticsuite/Standard/src/Example/Command/Cache/GetOtherCommand.php
+++ api/src/Elasticsuite/Standard/src/Example/Command/Cache/GetOtherCommand.php
@@ -5,9 +5,8 @@
  * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
  * versions in the future.
  *
- * @category  Elasticsuite
- * @package   Elasticsuite\Example
- * @author    Richard Bayet <richard.bayet@smile.fr>
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
  * @copyright 2022 Smile
  * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
  *            Unauthorized copying of this file, via any medium, is strictly prohibited.
