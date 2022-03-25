e7c2a233f690cd83e93103530a3cea6043fa3964
Richard BAYET <richard.bayet@smile.fr>
2022-03-25 17:30:43 +0100

    ESPP-19 Refresh && ESP-20 Alias (install index)

--- api/src/Elasticsuite/Standard/src/Index/Repository/Index/IndexRepositoryInterface.php
+++ api/src/Elasticsuite/Standard/src/Index/Repository/Index/IndexRepositoryInterface.php
@@ -40,9 +40,9 @@ interface IndexRepositoryInterface
      *
      * @param string       $indexName index name
      * @param array<mixed> $settings  index settings
-     * @param string|null  $alias     index alias to assign after index creation
+     * @param string[]     $aliases   index aliases to assign after index creation
      */
-    public function create(string $indexName, array $settings = [], string $alias = null): ?Index;
+    public function create(string $indexName, array $settings = [], array $aliases = []): ?Index;
 
     /**
      * Send bulk to index.
@@ -65,4 +65,54 @@ interface IndexRepositoryInterface
      * @param string $indexName index name
      */
     public function delete(string $indexName): void;
+
+    /**
+     * Get the alias/aliases of a given index.
+     *
+     * @param string $indexName index name
+     * @param string $alias     index alias name(s), accepts wildcards
+     *
+     * @return array<mixed>
+     */
+    public function getIndexAliases(string $indexName, string $alias = '*'): array;
+
+    /**
+     * Returns whether a given alias exists or a given alias is assigned to a given index.
+     * Returns all alias (globally or for a given index) if a wildcard is used for alias name.
+     *
+     * @param string      $alias     Index alias
+     * @param string|null $indexName Index name
+     */
+    public function aliasExists(string $alias, string $indexName = null): bool;
+
+    /**
+     * Update alias definitions.
+     *
+     * @param array<mixed> $aliasActions Alias actions
+     */
+    public function updateAliases(array $aliasActions): void;
+
+    /**
+     * Retrieves the mapping of one or several indices.
+     *
+     * @param string[]|string $indexName One or several indices names
+     *
+     * @return array<mixed>
+     */
+    public function getMapping(array|string $indexName): array;
+
+    /**
+     * Apply settings to the index.
+     *
+     * @param string       $indexName     index name
+     * @param array<mixed> $indexSettings index settings
+     */
+    public function putSettings(string $indexName, array $indexSettings): void;
+
+    /**
+     * Performs the force merge operation on one or several indices.
+     *
+     * @param string[]|string $indexName One or several indices names
+     */
+    public function forceMerge(array|string $indexName): void;
 }
---
+++ api/src/Elasticsuite/Standard/src/Index/Service/IndexOperation.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright {2022} Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+namespace Elasticsuite\Index\Service;
+
+use Elasticsuite\Index\Api\IndexSettingsInterface;
+use Elasticsuite\Index\Repository\Index\IndexRepositoryInterface;
+
+class IndexOperation
+{
+    public function __construct(
+        private IndexRepositoryInterface $indexRepository,
+        private IndexSettingsInterface $indexSettings,
+    ) {
+    }
+
+    /**
+     * Install index
+     * - apply definitive settings
+     * - add the correct index alias while removing it from the older index.
+     *
+     * @param string $indexName index name
+     */
+    public function installIndexByName(string $indexName): void
+    {
+        $this->indexRepository->refresh([$indexName]);
+        $this->indexRepository->putSettings($indexName, $this->indexSettings->getInstallIndexSettings());
+        $this->indexRepository->forceMerge($indexName);
+
+        $indexAlias = $this->getInstalledIndexAlias($indexName);
+        if (!empty($indexAlias)) {
+            $this->proceedInstallIndex($indexName, $indexAlias);
+        }
+        // TODO else throw an error ?
+    }
+
+    /**
+     * Proceed to the indices install :
+     *  1) First switch the alias to the new index
+     *  2) Remove old indices.
+     *
+     * @param string $indexName  Real index name
+     * @param string $indexAlias Index alias (must include catalog identifier)
+     */
+    public function proceedInstallIndex(string $indexName, string $indexAlias): void
+    {
+        $aliasActions = [];
+        $toDeleteIndices = [];
+
+        $aliasActions[] = [
+            'add' => ['index' => $indexName, 'alias' => $indexAlias],
+        ];
+        try {
+            $oldIndices = array_keys($this->indexRepository->getMapping($indexAlias));
+        } catch (\Elasticsearch\Common\Exceptions\Missing404Exception $e) {
+            $oldIndices = [];
+        }
+        foreach ($oldIndices as $oldIndexName) {
+            if ($oldIndexName != $indexName) {
+                $toDeleteIndices[] = $oldIndexName;
+                $aliasActions[] = ['remove' => ['index' => $oldIndexName, 'alias' => $indexAlias]];
+            }
+        }
+
+        if (!empty($aliasActions)) {
+            $this->indexRepository->updateAliases($aliasActions);
+        }
+
+        foreach ($toDeleteIndices as $toDeleteIndex) {
+            $this->indexRepository->delete($toDeleteIndex);
+        }
+    }
+
+    /**
+     * Return the index alias to apply to the installed index.
+     *
+     * @param string $indexName Index name
+     */
+    protected function getInstalledIndexAlias(string $indexName): string|null
+    {
+        $installIndexAlias = null;
+
+        $indexMetadataAliases = $this->indexRepository->getIndexAliases($indexName, '.*');
+        $entityType = $this->extractEntityFromAliases($indexMetadataAliases);
+        $catalogId = $this->extractCatalogIdFromAliases($indexMetadataAliases);
+        if (!empty($entityType) && !empty($catalogId)) {
+            $installIndexAlias = $this->indexSettings->getIndexAliasFromIdentifier($entityType, $catalogId);
+        }
+
+        return $installIndexAlias;
+    }
+
+    /**
+     * Extract original entity from index metadata aliases.
+     *
+     * @param string[] $indexMetadataAliases Index metadata aliases
+     */
+    protected function extractEntityFromAliases(array $indexMetadataAliases): string|null
+    {
+        $entityType = preg_filter('#^\.entity_(.+)$#', '$1', $indexMetadataAliases, 1);
+        if (!empty($entityType)) {
+            if (\is_array($entityType)) {
+                $entityType = current($entityType);
+            }
+        }
+
+        return $entityType;
+    }
+
+    /**
+     * Extract original catalog id from index metadata aliases.
+     *
+     * @param string[] $indexMetadataAliases Index metadata aliases
+     */
+    protected function extractCatalogIdFromAliases(array $indexMetadataAliases): int|null
+    {
+        $catalogId = preg_filter('#^\.catalog_(.+)$#', '$1', $indexMetadataAliases, 1);
+        if (!empty($catalogId)) {
+            if (\is_array($catalogId)) {
+                $catalogId = current($catalogId);
+            }
+            $catalogId = (int) $catalogId;
+        } else {
+            $catalogId = null;
+        }
+
+        return $catalogId;
+    }
+}
