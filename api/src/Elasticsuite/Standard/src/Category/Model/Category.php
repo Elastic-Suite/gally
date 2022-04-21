8c679b99f4d9c94556fb0ebc49027eb8733a87c7
Pierre Gauthier <pigau@smile.fr>
2022-04-21 16:01:26 +0200

    Facets Configuration #ESPP-229

---
+++ api/src/Elasticsuite/Standard/src/Category/Model/Category.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Category\Model;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ApiResource]
+class Category
+{
+    private int $id;
+
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
---
+++ api/src/Elasticsuite/Standard/src/Search/Validator/ProductFieldConstraintValidator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Search\Validator;
+
+use Elasticsuite\Metadata\Model\SourceField;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class ProductFieldConstraintValidator extends ConstraintValidator
+{
+    /**
+     * @param ?SourceField $value
+     *
+     * @return void
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof ProductFieldConstraint) {
+            throw new UnexpectedTypeException($constraint, ProductFieldConstraint::class);
+        }
+
+        if ($value && 'product' !== $value->getMetadata()->getEntity()) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ sourceFieldCode }}', $value->getName())
+                ->addViolation();
+        }
+    }
+}
