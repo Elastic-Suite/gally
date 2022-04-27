45c6ed9c061d8f39e44b6ac0faa3a5d30dfc47d9
thibo <thibaut.boudier@smile.fr>
2022-04-27 12:22:36 +0200

    feat: #ESPP-206 #ESPP-208 - Creation of menu + setup collapsing menu

---
+++ pwa/components/atoms/menu/MenuItemIcon.tsx
@@ -0,0 +1,147 @@
+import IonIcon from '~/components/atoms/IonIcon'
+import { makeStyles } from '@mui/styles'
+import { useSidebarState, useStore } from 'react-admin'
+
+/*
+ * Use of mui makeStyles to create multiple styles reusing theme fm react-admin
+ * see: https://mui.com/system/styles/basics/
+ */
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme.spacing(2),
+    textDecoration: 'unset',
+    color: 'inherit',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    '& ion-icon': {
+      color: theme.palette.menu.text500,
+    },
+  },
+  childPadding: {
+    paddingBottom: theme.spacing(1),
+  },
+  boldStyle: {
+    fontFamily: 'inter',
+    fontWeight: 600,
+    fontSize: 13,
+    lineHeight: '20px',
+    letterSpacing: 0.5,
+    textTransform: 'uppercase',
+    color: theme.palette.menu.text600,
+  },
+  lightStyle: {
+    fontFamily: 'inter',
+    fontWeight: 500,
+    fontSize: 14,
+    lineHeight: '20px',
+    color: theme.palette.menu.text500,
+  },
+  noChildHover: {
+    '&:hover': {
+      background: theme.palette.menu.hover,
+      borderRadius: theme.spacing(1),
+    },
+  },
+  lineActive: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: theme.palette.menu.active,
+    flex: 1,
+  },
+  indicatorLineActive: {
+    position: 'absolute',
+    right: 0,
+    width: 3,
+    height: 36,
+    background: theme.palette.menu.active,
+    boxShadow: '-2px 0px 4px rgba(63, 50, 230, 0.2)',
+    borderRadius: '5px 0px 0px 5px',
+  },
+  span: {
+    opacity: 1,
+    transition: 'all 500ms',
+  },
+  hide: {
+    opacity: 0,
+    height: 0,
+  },
+}))
+
+const MenuItemIcon = (props) => {
+  /*
+   * useStore from ReactAdmin to store data globally
+   * see: https://marmelab.com/react-admin/doc/4.0/Store.html
+   */
+  let [menuItemActive, setMenuItemActive] = useStore(`menuItemActive`, '')
+  const [sidebarState] = useSidebarState()
+  const [sidebarStateTimeout] = useStore('sidebarStateTimeout')
+
+  /*
+   * Function to update active menu item
+   */
+  const clickItem = () => {
+    setMenuItemActive(props.code)
+  }
+
+  const classes = useStyles()
+  let classNameRoot = classes.root
+  let classNameStyle = ''
+  if (props.lightStyle) {
+    classNameStyle += classes.lightStyle
+  } else {
+    classNameStyle += classes.boldStyle
+  }
+
+  /*
+   * Verification and return of specific data if the item has children
+   */
+  if (props.childPadding) {
+    classNameRoot += ' ' + classes.childPadding
+    return (
+      <div className={classNameRoot + ' ' + classNameStyle}>
+        <IonIcon name={props.code} style={{ width: 16, height: 16 }} />
+        <span
+          className={classes.span + (sidebarState ? '' : ' ' + classes.hide)}
+          style={sidebarStateTimeout ? { width: 0 } : null}
+        >
+          {props.label}
+        </span>
+      </div>
+    )
+  } else {
+    classNameRoot += ' ' + classes.noChildHover
+    return (
+      <div
+        className={
+          menuItemActive === props.code
+            ? classes.lineActive + ' ' + classNameStyle
+            : classNameStyle
+        }
+      >
+        <a
+          href={`#/${props.href}`}
+          className={classNameRoot}
+          onClick={clickItem}
+        >
+          <IonIcon name={props.code} style={{ width: 16, height: 16 }} />
+          <span
+            className={classes.span + (sidebarState ? '' : ' ' + classes.hide)}
+            style={sidebarStateTimeout ? { width: 0 } : null}
+          >
+            {props.label}
+          </span>
+        </a>
+        {menuItemActive === props.code && (
+          <div className={classes.indicatorLineActive} />
+        )}
+      </div>
+    )
+  }
+}
+
+export default MenuItemIcon
--- pwa/components/molecules/layout/CustomSidebar.tsx
+++ pwa/components/molecules/layout/CustomSidebar.tsx
@@ -1,10 +1,112 @@
-import { Menu, Sidebar } from 'react-admin'
+import { useSidebarState, useStore } from 'react-admin'
+import { useEffect } from 'react'
+import CustomMenu from '~/components/molecules/layout/CustomMenu'
+import LogoExtended from '~/assets/images/LogoBlinkExtended.svg'
+import LogoCollapse from '~/assets/images/LogoBlinkCollapse.svg'
+import { makeStyles } from '@mui/styles'
+import { Collapse } from '@mui/material'
+
+/*
+ * Use of mui makeStyles to create multiple styles reusing theme fm react-admin
+ * see: https://mui.com/system/styles/basics/
+ */
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'column',
+    maxWidth: 278,
+    minHeight: '100vh',
+    background: theme.palette.background.paper,
+    border: '1px solid #E2E6F3',
+  },
+  rootCollapsed: {
+    width: 'inherit',
+  },
+  imgContainer: {
+    position: 'relative',
+    paddingBottom: theme.spacing(6),
+    paddingTop: theme.spacing(3),
+    paddingLeft: theme.spacing(2.5),
+  },
+  imgExtended: {
+    position: 'absolute',
+    width: 104,
+    transition: 'opacity 500ms',
+  },
+  imgCollapse: {
+    position: 'absolute',
+    width: 31,
+    transition: 'opacity 500ms',
+  },
+  imgNotActive: {
+    opacity: 0,
+  },
+  menu: {
+    marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+  },
+}))
 
 const CustomSidebar = (props) => {
+  /*
+   * useStore from ReactAdmin to store data globally
+   * see: https://marmelab.com/react-admin/doc/4.0/Store.html
+   */
+  const [menu, setMenu] = useStore('menu', { hierarchy: [] })
+  const [sidebarState] = useSidebarState()
+
+  /*
+   * Fetch data from /menu to get create menu items dynamically
+   */
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/menu')
+      const json = await res.json()
+      setMenu(json)
+    }
+    fetchData()
+  }, [setMenu])
+
+  const classes = useStyles()
+
+  /*
+   * Use of Collapse from mui to collapse sidebar when button is clicked
+   * see: https://mui.com/material-ui/transitions/#collapse
+   */
   return (
-    <Sidebar classes={props.className}>
-      <Menu hasDashboard={!!props.dashboard} />
-    </Sidebar>
+    <Collapse
+      in={sidebarState}
+      orientation={'horizontal'}
+      collapsedSize={66}
+      timeout={sidebarState ? 100 : 1000}
+    >
+      <div
+        className={
+          classes.root + (sidebarState ? '' : ' ' + classes.rootCollapsed)
+        }
+      >
+        <div className={classes.imgContainer}>
+          <img
+            src={LogoExtended.src}
+            className={
+              classes.imgExtended +
+              (sidebarState ? '' : ' ' + classes.imgNotActive)
+            }
+            alt={LogoExtended.name}
+          />
+          <img
+            src={LogoCollapse.src}
+            className={
+              classes.imgCollapse +
+              (sidebarState ? ' ' + classes.imgNotActive : '')
+            }
+            alt={LogoCollapse.name}
+          />
+        </div>
+        <CustomMenu className={classes.menu} />
+      </div>
+    </Collapse>
   )
 }
 
