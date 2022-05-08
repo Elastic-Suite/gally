9bc2ef37a6381c2c8e179c18f5dd6fcf0332cf91
Richard BAYET <richard.bayet@smile.fr>
2022-05-08 17:33:58 +0200

    ESPP-197 Adapter and Response objects

---
+++ api/src/Elasticsuite/Standard/src/Search/Elasticsearch/Adapter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Search\Elasticsearch;
+
+use Elasticsearch\Client;
+use Elasticsuite\Search\Elasticsearch\Adapter\Common\Request;
+use Elasticsuite\Search\Elasticsearch\Adapter\Common\Response;
+use Exception;
+use Psr\Log\LoggerInterface;
+
+class Adapter
+{
+    public function __construct(
+        private Request\Mapper $requestMapper,
+        private Client $client,
+        private LoggerInterface $logger
+    ) {
+    }
+
+    public function search(RequestInterface $request): ResponseInterface
+    {
+        try {
+            $searchRequest = [
+                'index' => $request->getIndex(),
+                'body' => $this->requestMapper->assembleSearchRequest($request),
+            ];
+            $searchResponse = $this->client->search($searchRequest);
+        } catch (Exception $e) {
+            $searchResponse = [];
+            $this->logger->error($e->getMessage());
+        }
+
+        return new Response\QueryResponse($searchResponse);
+    }
+}
---
+++ api/src/Elasticsuite/Standard/src/Search/Tests/Unit/Elasticsearch/Adapter/Common/Response/QueryResponseTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Search\Tests\Unit\Elasticsearch\Adapter\Common\Response;
+
+use Elasticsuite\Search\Elasticsearch\Adapter\Common\Response;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class QueryResponseTest extends KernelTestCase
+{
+    /**
+     * @dataProvider queryResponseDocumentsOnlyDataProvider
+     *
+     * @param array $searchResponse    Raw search response from Elasticsearch
+     * @param int   $expectedDocsCount Expected documents count in the query response
+     */
+    public function testTraversableCountable(array $searchResponse, int $expectedDocsCount): void
+    {
+        $queryResponse = new Response\QueryResponse($searchResponse);
+        $this->assertCount($expectedDocsCount, $queryResponse);
+        $this->assertContainsOnlyInstancesOf(Response\Document::class, $queryResponse);
+    }
+
+    public function queryResponseDocumentsOnlyDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'hits' => [
+                        'hits' => [
+                            [
+                                'id' => '1',
+                                '_score' => 1.0,
+                                '_source' => [
+                                    ['field' => 'value'],
+                                ],
+                            ],
+                        ],
+                        'total' => 1,
+                    ],
+                ],
+                1,
+            ],
+            [
+                [
+                    'hits' => [
+                        'hits' => [
+                            [
+                                'id' => '1',
+                                '_score' => 1.1,
+                                '_source' => [
+                                    ['field' => 'value'],
+                                ],
+                            ],
+                            [
+                                'id' => '2',
+                                '_score' => 1.0,
+                                '_source' => [
+                                    ['field1' => 'value1', 'field2' => 'value2'],
+                                ],
+                            ],
+                        ],
+                        'total' => ['value' => 2],
+                    ],
+                ],
+                2,
+            ],
+            [
+                [
+                    'hits' => [
+                        'hits' => [],
+                        'total' => 0,
+                    ],
+                ],
+                0,
+            ],
+            [
+                [],
+                0,
+            ],
+        ];
+    }
+}
