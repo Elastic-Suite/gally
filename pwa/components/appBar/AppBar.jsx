24a758660acc17f85d21ad21d09c365b2927dbaa
Adrien Meynard <adrien.meynard@smile.fr>
2022-05-23 17:22:30 +0200

    update: add in app bar: breadCrum and user

---
+++ pwa/components/appBar/AppBar.jsx
@@ -0,0 +1,46 @@
+import { makeStyles } from '@mui/styles'
+import BreadCrumb from '~/components/appBar/breadcrumb/BreadCrum'
+import User from '~/components/appBar/user/User'
+import { useSidebarState } from 'react-admin'
+const useStylesAppBar = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 32px',
+        position: 'fixed',
+        right: 0,
+        zIndex: '9',
+        height: '65px',
+        backgroundColor: '#fafafb',
+
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            height: '1px',
+            background: theme.palette.colors.neutral['300'],
+            bottom: 0,
+            width: `calc(100% - 64px)`,
+        },
+    },
+}))
+
+const AppBar = () => {
+    const [sidebarState, setSidebarState] = useSidebarState()
+
+    const appbarstyle = useStylesAppBar()
+    return (
+        <div
+            className={appbarstyle.root}
+            style={{
+                left: sidebarState ? '279px' : '66px',
+            }}
+        >
+            <BreadCrumb />
+            <User />
+        </div>
+    )
+}
+
+export default AppBar
