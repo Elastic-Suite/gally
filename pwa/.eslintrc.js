e502af4656ad4442c91145f9bd631d88e22ac24b
Tony CABAYE <tony.cabaye@smile.fr>
2022-06-21 17:57:08 +0200

    chore: addd eslint rules

--- pwa/.eslintrc.js
+++ pwa/.eslintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'prettier',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@next/next/recommended',
-    'plugin:storybook/recommended',
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -20,9 +6,41 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    // tsconfigRootDir: __dirname,
+    // project: ['./tsconfig.json'],
   },
-  plugins: ['react', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', '@typescript-eslint'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
   rules: {
     'jsx-a11y/anchor-is-valid': 0,
+    '@typescript-eslint/naming-convention': [
+      2,
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
   },
 }
