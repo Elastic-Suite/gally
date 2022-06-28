f11295e849806a3ba86fb6a02006205d63a737bf
Tony CABAYE <tony.cabaye@smile.fr>
2022-06-28 11:39:28 +0200

    chore: add language switcher in storybook

---
+++ pwa/.storybook/I18nProvider.tsx
@@ -0,0 +1,21 @@
+import { ReactChild, useEffect } from 'react'
+import { I18nextProvider } from 'react-i18next'
+
+import i18n from './i18n'
+
+interface IProps {
+  children: ReactChild
+  locale: string
+}
+
+function I18nProvider(props: IProps) {
+  const { children, locale } = props
+
+  useEffect(() => {
+    i18n.changeLanguage(locale)
+  }, [locale])
+
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+}
+
+export default I18nProvider
