1bd596f6426e4ec5c221a19e293bc0bee973b0ab
Tony CABAYE <tony.cabaye@smile.fr>
2022-07-01 15:41:03 +0200

    feat: add Checkbox and Dropdown multiple

--- pwa/components/atoms/form/Dropdown.stories.tsx
+++ pwa/components/atoms/form/Dropdown.stories.tsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import { ComponentStory, ComponentMeta } from '@storybook/react'
-import DropDownComponent from './DropDown'
+import { ComponentMeta } from '@storybook/react'
+import DropDownComponent, { IMultiSelectProps, ISelectProps } from './DropDown'
 
 export default {
   title: 'Atoms/Form',
   component: DropDownComponent,
 } as ComponentMeta<typeof DropDownComponent>
 
-const Template: ComponentStory<typeof DropDownComponent> = (args) => {
-  const [value, setValue] = useState(0)
+export const Dropdown = (args) => {
+  const [value, setValue] = useState('')
   const handleChange = (value) => setValue(value)
-
   return (
-    <>
-      <DropDownComponent {...args} onChange={handleChange} value={value} />
-      <p>Selected value: {value}</p>
-    </>
+    <DropDownComponent
+      {...(args as ISelectProps)}
+      onChange={handleChange}
+      value={value}
+    />
   )
 }
-
-export const Dropdown = Template.bind({})
 Dropdown.args = {
-  label: 'Label',
-  required: false,
   disabled: false,
+  label: 'Label',
   options: [
-    { label: 'Select an item', value: 0 },
     { label: 'Ten', value: 10 },
     { label: 'Twenty', value: 20 },
     { label: 'Thirty', value: 30 },
+    { label: 'Fourty', value: 40, disabled: true },
   ],
+  required: false,
+}
+
+export const DropdownMultiple = (args) => {
+  const [multiValue, setMultiValue] = useState([])
+  const handleChange = (value) => setMultiValue(value)
+  return (
+    <DropDownComponent
+      {...(args as IMultiSelectProps)}
+      multiple
+      onChange={handleChange}
+      value={multiValue}
+    />
+  )
+}
+DropdownMultiple.args = {
+  disabled: false,
+  label: 'Label',
+  options: [
+    { label: 'Ten', value: 10 },
+    { label: 'Twenty', value: 20 },
+    { label: 'Thirty', value: 30 },
+    { label: 'Fourty', value: 40, disabled: true },
+  ],
+  required: false,
 }
