a44123e08710cf5544ab0e58eb6ab9e0effa15e4
Adrien Meynard <adrien.meynard@smile.fr>
2022-07-04 17:17:50 +0200

    ESPP_215 update style

--- pwa/components/atoms/modals/PopIn.tsx
+++ pwa/components/atoms/modals/PopIn.tsx
@@ -28,6 +28,7 @@ const CustomTitle = styled('div')(({ theme }) => ({
   fontWeight: '400',
   color: theme.palette.colors.neutral[900],
   fontSize: '18px',
+  fontFamily: 'Inter',
   lineHeight: '28px',
   marginBottom: theme.spacing(7),
 }))
