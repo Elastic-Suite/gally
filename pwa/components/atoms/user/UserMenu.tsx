acd633e25182d97598b537b9bc9e9b62181edd53
Adrien Meynard <adrien.meynard@smile.fr>
2022-07-05 10:21:10 +0200

    fix: styles UserMenu

--- pwa/components/atoms/user/UserMenu.tsx
+++ pwa/components/atoms/user/UserMenu.tsx
@@ -4,51 +4,7 @@ import { makeStyles } from '@mui/styles'
 import IonIcon from '~/components/atoms/IonIcon/IonIcon'
 import CloseComponent from '../closeComponent/CloseComponent'
 import { Box } from '@mui/material'
-
-const useStylesUserMenuShow = makeStyles((theme: Theme) => ({
-  typoTexte: {
-    fontStyle: 'normal',
-    fontWeight: 600,
-    fontSize: '12px',
-    lineHeight: '18px',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(1),
-    fontFamily: 'Inter',
-  },
-  typoUsername: {
-    fontWeight: '600',
-    color: theme.palette.colors.neutral['800'],
-  },
-  typoEmail: {
-    fontWeight: '400',
-    color: theme.palette.colors.neutral['600'],
-  },
-  typoBasic: {
-    fontWeight: '400',
-    color: theme.palette.colors.neutral['800'],
-  },
-  hr: {
-    width: '100%',
-    border: '1px solid',
-    margin: 0,
-    borderColor: theme.palette.colors.neutral['300'],
-  },
-}))
-
-function UserMenuShow() {
-  const usermenushowstyle = useStylesUserMenuShow()
-
-  return (
-    <div className={usermenushowstyle.typoTexte}>
-      <div className={usermenushowstyle.typoUsername}>Admin name</div>
-      <div className={usermenushowstyle.typoEmail}>adminame@mail.com</div>
-      <hr className={usermenushowstyle.hr} />
-      <div className={usermenushowstyle.typoBasic}>Account</div>
-      <div className={usermenushowstyle.typoBasic}>Log out</div>
-    </div>
-  )
-}
+import UserMenuShow from './UserMenuShow'
 
 const useStylesUserMenu = makeStyles((theme: Theme) => ({
   user: {
