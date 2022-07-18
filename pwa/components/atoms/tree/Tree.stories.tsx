41044e40f3334f86104ca26da7ca525cd4b932e9
Adrien Meynard <adrien.meynard@smile.fr>
2022-07-18 18:05:34 +0200

    ESPP_250 fix : eslint

---
+++ pwa/components/atoms/tree/Tree.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Tree from './Tree'
+import categories from '../../../public/mocks/categories.json'
+
+export default {
+  title: 'Atoms/Tree',
+  component: Tree,
+} as ComponentMeta<typeof Tree>
+
+const Template: ComponentStory<typeof Tree> = (args) => <Tree {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  data: categories,
+}
