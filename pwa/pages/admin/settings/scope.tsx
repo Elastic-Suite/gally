b87a3441adffbe6f863bd5c2fa033e18f59871d0
Adrien Meynard <adrien.meynard@smile.fr>
2022-07-20 15:52:55 +0200

    ESPP_342 fix : i18n + refacto + review pr

--- pwa/pages/admin/settings/scope.tsx
+++ pwa/pages/admin/settings/scope.tsx
@@ -5,10 +5,12 @@ import ActiveLocales from '~/components/molecules/layout/scope/ActiveLocales'
 import Catalogs from '~/components/molecules/layout/scope/Catalogs'
 import { IHydraResponse } from '~/types'
 import { useApiFetch } from '~/hooks/useApi'
+import { firstLetterUppercase } from '~/services/format'
+import { useTranslation } from 'next-i18next'
 
 const Scope = () => {
   const [catalogsFields] = useApiFetch<IHydraResponse>('/catalogs')
-
+  const { t } = useTranslation('common')
   if (catalogsFields.error) {
     return catalogsFields.error.toString()
   } else if (!catalogsFields.data) {
@@ -17,13 +19,16 @@ const Scope = () => {
 
   return (
     <>
-      <Title name="Catalog" />
+      <Title name={firstLetterUppercase(t('catalog_one'))} />
       <CustomTabs
-        labels={['Scope', 'Searchable and filtrable attributes']}
+        labels={[t('scope'), t('searchableAndFiltrableAttributes')]}
         contents={[
           <SubTabs
             key="labels"
-            labels={['Catalogs', 'Actives locales']}
+            labels={[
+              firstLetterUppercase(t('catalog_other')),
+              firstLetterUppercase(t('activeLocale_other')),
+            ]}
             contents={[
               <Catalogs key={'Catalogs'} content={catalogsFields.data} />,
               <ActiveLocales
@@ -32,7 +37,7 @@ const Scope = () => {
               />,
             ]}
           />,
-          'Content of Searchable and filtrable attributes',
+          t('contentOfSearchableAndFiltrableAttributes'),
         ]}
       />
     </>
