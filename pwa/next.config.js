5751847fc37d1599d4b8cf230d5efac26ab12285
Tony CABAYE <tony.cabaye@smile.fr>
2022-07-22 17:19:45 +0200

    fix: running local env

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -2,7 +2,37 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
+  headers() {
+    if (process.env.NEXT_PUBLIC_LOCAL) {
+      return [
+        {
+          source: '/mocks/:mock*',
+          headers: [
+            {
+              key: 'link',
+              value:
+                '<http://localhost:3000/mocks/docs.jsonld>; rel="http://www.w3.org/ns/hydra/core#apiDocumentation"',
+            },
+            {
+              key: 'Content-Type',
+              value: 'application/ld+json',
+            },
+          ],
+        },
+      ]
+    }
+    return []
+  },
   i18n,
+  redirects() {
+    return [
+      {
+        source: '/mocks',
+        destination: '/mocks/index.json',
+        permanent: true,
+      },
+    ]
+  },
   serverRuntimeConfig: {
     NEXT_PUBLIC_ENTRYPOINT:
       process.env.NEXT_PUBLIC_ENTRYPOINT || 'https://localhost',
