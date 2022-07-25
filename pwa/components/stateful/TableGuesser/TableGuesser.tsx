14e9426395fc7af8395be15ad0e31e73e9741052
Tony CABAYE <tony.cabaye@smile.fr>
2022-07-25 16:41:24 +0200

    feat: manage pagination and filters

--- pwa/components/stateful/TableGuesser/TableGuesser.tsx
+++ pwa/components/stateful/TableGuesser/TableGuesser.tsx
@@ -1,30 +1,37 @@
-import { MouseEvent } from 'react'
 import { Resource } from '@api-platform/api-doc-parser'
 
+import { defaultPageSize } from '~/constants'
+import { useApiHeaders } from '~/hooks'
 import { IHydraMember, IHydraResponse, ITableRow } from '~/types'
 
 import PagerTable from '~/components/organisms/PagerTable/PagerTable'
-import { useApiHeaders } from '~/hooks'
 
 interface IProps<T extends IHydraMember> {
   apiData: IHydraResponse<T>
-  onPageChange: (
-    event: MouseEvent<HTMLButtonElement> | null,
-    page: number
-  ) => void
+  currentPage?: number
+  onPageChange: (page: number) => void
   resource: Resource
+  rowsPerPage?: number
+  rowsPerPageOptions?: number[]
 }
 
 function TableGuesser<T extends IHydraMember>(props: IProps<T>): JSX.Element {
-  const { apiData, onPageChange, resource } = props
+  const {
+    apiData,
+    currentPage,
+    onPageChange,
+    resource,
+    rowsPerPage,
+    rowsPerPageOptions,
+  } = props
   const tableHeaders = useApiHeaders(resource)
 
   return (
     <PagerTable
-      currentPage={0}
+      currentPage={currentPage ?? 0}
       onPageChange={onPageChange}
-      rowsPerPage={50}
-      rowsPerPageOptions={[]}
+      rowsPerPage={rowsPerPage ?? defaultPageSize}
+      rowsPerPageOptions={rowsPerPageOptions ?? []}
       tableHeaders={tableHeaders}
       tableRows={apiData['hydra:member'] as unknown as ITableRow[]}
       totalPages={apiData['hydra:totalItems']}
--- pwa/constants/api.ts
+++ pwa/constants/api.ts
@@ -3,3 +3,9 @@ if (url && String(url).endsWith('/')) {
   url = url.slice(0, -1)
 }
 export const apiUrl = url
+
+export const currentPage = 'currentPage'
+export const pageSize = 'pageSize'
+export const usePagination = 'pagination'
+
+export const defaultPageSize = 50
