9708c87d22d4877f70bec48f99eb2217fbdddf72
Tony CABAYE <tony.cabaye@smile.fr>
2022-07-29 15:11:04 +0200

    refactor: add hook for tab management with tests

--- pwa/components/stateful-pages/SettingsScope/SettingsScope.tsx
+++ pwa/components/stateful-pages/SettingsScope/SettingsScope.tsx
@@ -1,9 +1,7 @@
 import { useMemo } from 'react'
 import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
 
-import { useApiList, useResource } from '~/hooks'
-import { getRouterPath } from '~/services'
+import { useApiList, useResource, useTabs } from '~/hooks'
 import { ICatalog, IHydraResponse, IRouterTab } from '~/types'
 
 import SubTabs from '~/components/atoms/subTabs/SubTabs'
@@ -11,12 +9,11 @@ import Catalogs from '~/components/molecules/layout/scope/Catalogs'
 import ActiveLocales from '~/components/molecules/layout/scope/ActiveLocales'
 
 function SettingsScope(): JSX.Element {
+  const { t } = useTranslation('catalog')
   const resourceName = 'catalogs'
   const resource = useResource(resourceName)
   const [catalogsFields] = useApiList<IHydraResponse<ICatalog>>(resource, false)
 
-  const { t } = useTranslation('catalog')
-  const router = useRouter()
   const routerTabs: IRouterTab[] = useMemo(
     () => [
       {
@@ -37,10 +34,7 @@ function SettingsScope(): JSX.Element {
     ],
     [catalogsFields.data, t]
   )
-  const pathname = getRouterPath(router.asPath)
-  const activeTab =
-    routerTabs.find((tab) => tab.url === pathname) ??
-    routerTabs.find((tab) => tab.default)
+  const [activeTab, handleTabChange] = useTabs(routerTabs)
   const { id } = activeTab
 
   if (catalogsFields.error) {
@@ -49,15 +43,12 @@ function SettingsScope(): JSX.Element {
     return null
   }
 
-  function handleChange(id: number): void {
-    const tab = routerTabs.find((tab) => tab.id === id)
-    if (tab) {
-      router.push(tab.url, undefined, { shallow: true })
-    }
-  }
-
   return (
-    <SubTabs defaultActiveId={id} onChange={handleChange} tabs={routerTabs} />
+    <SubTabs
+      defaultActiveId={id}
+      onChange={handleTabChange}
+      tabs={routerTabs}
+    />
   )
 }
 
