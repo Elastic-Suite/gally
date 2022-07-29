2d847f4ff760007b7795277a50f8d4a81eee545d
Tony CABAYE <tony.cabaye@smile.fr>
2022-07-29 13:42:36 +0200

    fix: add active parameter to avoid redirection if tab is not active

--- pwa/components/molecules/layout/tabs/TabsAndSubTabs.stories.tsx
+++ pwa/components/molecules/layout/tabs/TabsAndSubTabs.stories.tsx
@@ -13,9 +13,9 @@ const Template: ComponentStory<typeof CustomTabs> = (args) => (
 export const Default = Template.bind({})
 Default.args = {
   tabs: [
-    { label: 'Tab 1', content: 'Hello World 1', id: 0 },
-    { label: 'Tab 2', content: 'Hello World 2', id: 1 },
-    { label: 'Tab 3', content: 'Hello World 3', id: 2 },
-    { label: 'Tab 4', content: 'Hello World 4', id: 3 },
+    { label: 'Tab 1', Component: (): JSX.Element => <>Hello World 1</>, id: 0 },
+    { label: 'Tab 2', Component: (): JSX.Element => <>Hello World 2</>, id: 1 },
+    { label: 'Tab 3', Component: (): JSX.Element => <>Hello World 3</>, id: 2 },
+    { label: 'Tab 4', Component: (): JSX.Element => <>Hello World 4</>, id: 3 },
   ],
 }
