b1fcecaa1c1950df42bb59be83f9620ea2f2335f
Ponefar <adrienwolf@laposte.net>
2022-08-01 12:09:35 +0200

    ESPP_add_btn_storybook

---
+++ pwa/components/atoms/form/Switch.test.tsx
@@ -0,0 +1,36 @@
+import Switch from '@mui/material/switch'
+import { renderWithProviders } from '~/services'
+
+describe('Switch match snapshot', () => {
+  it('Switch desabled', () => {
+    const { container } = renderWithProviders(<Switch disabled />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Switch desabled false', () => {
+    const { container } = renderWithProviders(<Switch disabled={false} />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Switch defaultChecked true', () => {
+    const { container } = renderWithProviders(<Switch defaultChecked />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Switch defaultChecked false', () => {
+    const { container } = renderWithProviders(<Switch defaultChecked={false} />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Switch value is Hello', () => {
+    const { container } = renderWithProviders(<Switch value="Hello" />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Switch onChange is console.log("Hello")', () => {
+    const { container } = renderWithProviders(
+      <Switch onChange={(): void => console.log('Hello')} />
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
