c5d15f16bd40776cb3ab0b09bd8f403eb7a773f7
Ponefar <adrienwolf1@gmail.com>
2022-08-03 01:38:20 +0200

    ESPP_add_btn_storybook : refacto

--- pwa/components/atoms/form/RadioGroup.test.tsx
+++ pwa/components/atoms/form/RadioGroup.test.tsx
@@ -2,11 +2,56 @@ import { renderWithProviders } from '~/services'
 import RadioGroup from './RadioGroup'
 
 describe('Switch match snapshot', () => {
-  it('Radio group simple', () => {
+  it('Radio group defaultChecked true with default', () => {
     const { container } = renderWithProviders(
       <RadioGroup
         name="radio-buttons-group"
-        defaultChecked="female"
+        defaultChecked
+        row
+        options={[
+          { value: 'male', label: 'Label One', disabled: true },
+          { value: 'female', label: 'Label Two', default: true },
+        ]}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Radio group defaultChecked true without default', () => {
+    const { container } = renderWithProviders(
+      <RadioGroup
+        name="radio-buttons-group"
+        defaultChecked
+        row
+        options={[
+          { value: 'male', label: 'Label One', disabled: true },
+          { value: 'female', label: 'Label Two' },
+        ]}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Radio group defaultChecked False with default true', () => {
+    const { container } = renderWithProviders(
+      <RadioGroup
+        name="radio-buttons-group"
+        defaultChecked={false}
+        row
+        options={[
+          { value: 'male', label: 'Label One', disabled: true },
+          { value: 'female', label: 'Label Two', default: true },
+        ]}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Radio group defaultChecked False without default', () => {
+    const { container } = renderWithProviders(
+      <RadioGroup
+        name="radio-buttons-group"
+        defaultChecked={false}
         row
         options={[
           { value: 'male', label: 'Label One', disabled: true },
