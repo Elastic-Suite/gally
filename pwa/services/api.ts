3a81c40fc28502dce0c9a40f7a0d18307549598a
Tony CABAYE <tony.cabaye@smile.fr>
2022-08-05 12:14:22 +0200

    feat: manage new translation header

--- pwa/services/api.ts
+++ pwa/services/api.ts
@@ -64,7 +64,7 @@ export async function fetchApi<T>(
     ...options,
     headers: {
       ...options.headers,
-      'Accept-Language': language,
+      'Elasticsuite-Language': language,
     },
   })
   return json
