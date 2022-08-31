f04eacde093b90de4396e1388ed677320e5667db
Tony CABAYE <tony.cabaye@smile.fr>
2022-08-31 17:04:05 +0200

    fix: add product metadata filter for attribute grid

--- pwa/components/stateful-pages/SettingsAttributes/SettingsAttributes.tsx
+++ pwa/components/stateful-pages/SettingsAttributes/SettingsAttributes.tsx
@@ -2,10 +2,11 @@ import { ITabContentProps } from '~/types'
 
 import CommonGridFromSourceField from '../CommonGridFromSourceField/CommonGridFromSourceField'
 
+const fixedFilters = { 'metadata.entity': 'product' }
+
 function SettingsAttributes(props: ITabContentProps): JSX.Element {
   const { active } = props
-
-  return <CommonGridFromSourceField active={active} />
+  return <CommonGridFromSourceField active={active} filters={fixedFilters} />
 }
 
 export default SettingsAttributes
