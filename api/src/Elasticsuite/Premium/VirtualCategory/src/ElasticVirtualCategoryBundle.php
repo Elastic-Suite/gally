2b3f4fbdc0afacbc3705c7402aabbc732c44aa9d
Pierre Gauthier <pigau@smile.fr>
2022-08-31 18:20:05 +0200

    Dual Version - Premium Namespace #ESPP-374

---
+++ api/src/Elasticsuite/Premium/VirtualCategory/src/ElasticVirtualCategoryBundle.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\VirtualCategory;
+
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ElasticVirtualCategoryBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        $mappings = [
+            realpath(__DIR__ . '/Resources/config/doctrine') => 'Elasticsuite\VirtualCategory\Model',
+        ];
+
+        $container->addCompilerPass(
+            DoctrineOrmMappingsPass::createXmlMappingDriver(
+                $mappings,
+                ['doctrine.orm.entity_manager'],
+                false
+            )
+        );
+    }
+}
