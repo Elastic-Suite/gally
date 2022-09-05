b4d6fe71be974485c66f5b7d5e471b7d28f77080
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-05 17:21:02 +0200

    test: add unit tests

---
+++ pwa/services/options.test.ts
@@ -0,0 +1,23 @@
+import { RuleAttributeOperator, RuleCombinationOperator } from '~/types'
+
+import { getOptionsFromEnum } from './options'
+
+describe('Options service', () => {
+  describe('getOptionsFromEnum', () => {
+    it('should get the options from enum', () => {
+      expect(getOptionsFromEnum(RuleCombinationOperator, (x) => x)).toEqual([
+        { value: 'all', label: 'ALL' },
+        { value: 'any', label: 'ANY' },
+      ])
+      expect(getOptionsFromEnum(RuleAttributeOperator, (x) => x)).toEqual([
+        { value: 'is_one_of', label: 'IS_ONE_OF' },
+        { value: 'is_not_one_of', label: 'IS_NOT_ONE_OF' },
+        { value: 'gte', label: 'GTE' },
+        { value: 'lte', label: 'LTE' },
+        { value: 'eq', label: 'EQ' },
+        { value: 'neq', label: 'NEQ' },
+        { value: 'is', label: 'IS' },
+      ])
+    })
+  })
+})
---
+++ pwa/services/rules.test.ts
@@ -0,0 +1,19 @@
+import { attributeRule, combinationRule } from '~/mocks'
+
+import { isAttributeRule, isCombinationRule } from './rules'
+
+describe('Rules service', () => {
+  describe('isAttributeRule', () => {
+    it('should check if rule is an attribute rule', () => {
+      expect(isAttributeRule(attributeRule)).toEqual(true)
+      expect(isAttributeRule(combinationRule)).toEqual(false)
+    })
+  })
+
+  describe('isCombinationRule', () => {
+    it('should check if rule is a combination rule', () => {
+      expect(isCombinationRule(attributeRule)).toEqual(false)
+      expect(isCombinationRule(combinationRule)).toEqual(true)
+    })
+  })
+})
