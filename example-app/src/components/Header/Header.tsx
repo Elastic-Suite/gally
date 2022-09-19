c7dcc013a80c7077e42fbf544ae2666a0df1a080
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-19 11:41:36 +0200

    feat: initialize router, header and menu

---
+++ example-app/src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Link } from 'react-router-dom'
+
+interface IProps {
+  onMenuToggle: () => void
+}
+
+function Header(props: IProps): JSX.Element {
+  const { onMenuToggle } = props
+  return (
+    <AppBar component="nav">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={onMenuToggle}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component={Link}
+          to="/"
+          variant="h6"
+        >
+          Example App{' '}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
