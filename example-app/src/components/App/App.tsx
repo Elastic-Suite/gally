2a10d8b217f33bee293729ba2a59ce1a3396cadb
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-19 11:41:36 +0200

    feat: initialize router, header and menu

---
+++ example-app/src/components/App/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Homepage from '../../pages/Homepage/Homepage'
+
+import Header from '../Header/Header'
+import Menu from '../Menu/Menu'
+
+function App():JSX.Element {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  function handleMenuToggle(): void {
+    setMenuOpen(prevState => !prevState)
+  }
+
+  return (
+    <BrowserRouter>
+      <Header onMenuToggle={handleMenuToggle}/>
+      <Menu menuOpen={menuOpen} onMenuToggle={handleMenuToggle}/>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
