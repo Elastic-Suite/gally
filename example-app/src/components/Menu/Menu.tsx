5c38391c5d8a01d72894388305ebf5ca9e1f3631
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-19 11:41:36 +0200

    feat: initialize router, header and menu

---
+++ example-app/src/components/Menu/Menu.tsx
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Drawer as MuiDrawer,
+  Typography,
+  styled,
+} from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+
+const navItems = ['Home', 'About', 'Contact']
+
+const Drawer = styled(MuiDrawer)(({ theme }) => ({
+  '& .MuiDrawer-paper': {
+    padding: `0 ${theme.spacing(3)}`,
+  },
+}))
+
+interface IProps {
+  menuOpen: boolean
+  onMenuToggle: () => void
+}
+
+function Menu(props: IProps): JSX.Element {
+  const { menuOpen, onMenuToggle } = props
+
+  return (
+    <Box component="nav">
+      <Drawer
+        variant="temporary"
+        open={menuOpen}
+        onClose={onMenuToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+      >
+        <Box onClick={onMenuToggle} sx={{ textAlign: 'center' }}>
+          <Typography component={RouterLink} to="/" sx={{ display: 'inline-block', my: 1 }} variant="h6">
+            Example App
+          </Typography>
+          <Divider />
+          <List dense>
+            {navItems.map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemButton component={RouterLink} to={item}>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  )
+}
+
+export default Menu
---
+++ example-app/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,5 @@
+function Homepage():JSX.Element {
+  return (<div>Homepage</div>)
+}
+
+export default Homepage
