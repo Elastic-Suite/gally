31ce3ba5c2609faae527db9ab4b04d524fe2cafd
Ponefar <adrienwolf1@gmail.com>
2022-09-20 17:20:16 +0200

    ESPP-367 update : translation for title and add modal + fix bug : add filter to request API, search filter

--- pwa/pages/admin/search/configuration/attributes.tsx
+++ pwa/pages/admin/search/configuration/attributes.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import { searchableAttributeUrl } from '~/constants'
 import { breadcrumbContext } from '~/contexts'
 import { withAuth } from '~/hocs'
+import Alert from '~/components/atoms/Alert/Alert'
 
 import PageTitle from '~/components/atoms/PageTitle/PageTitle'
 import CommonGridFromSourceField from '~/components/stateful-pages/CommonGridFromSourceField/CommonGridFromSourceField'
@@ -14,6 +15,8 @@ const pagesSlug = ['search', 'configuration', 'attributes']
 const fixedFilters = { 'metadata.entity': 'product' }
 
 function Attributes(): JSX.Element {
+  const [visibleAlertAttributes, setVisibleAlertAttributes] = useState(true)
+
   const router = useRouter()
   const [, setBreadcrumb] = useContext(breadcrumbContext)
 
@@ -21,7 +24,7 @@ function Attributes(): JSX.Element {
     setBreadcrumb(pagesSlug)
   }, [router.query, setBreadcrumb])
 
-  const { t } = useTranslation('common')
+  const { t } = useTranslation('attributes')
 
   const title = t(pagesSlug.slice(-1).flat().pop())
 
@@ -31,6 +34,11 @@ function Attributes(): JSX.Element {
         <title>{title}</title>
       </Head>
       <PageTitle title={title} />
+      {visibleAlertAttributes ? <Alert
+          message={t('attributesAlert')}
+          onClose={(): void => setVisibleAlertAttributes(false)}
+          style={{ marginBottom: '16px' }}
+        /> : null}
       <CommonGridFromSourceField
         active={false}
         urlParams={searchableAttributeUrl}
