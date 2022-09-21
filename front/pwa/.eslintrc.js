0106460a0fbae1f6a446ea990f467d1e8839141d
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-21 15:09:50 +0200

    chore: configure husky

--- front/pwa/.eslintrc.js
+++ front/pwa/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['../.eslintrc.js', 'plugin:@next/next/recommended'],
+  extends: [
+    '../.eslintrc.js',
+    'plugin:@next/next/recommended',
+    'plugin:storybook/recommended',
+  ],
 }
