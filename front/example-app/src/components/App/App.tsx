2d7066037638da9e39d7fe8cebfb386f7e4a416f
Tony CABAYE <tony.cabaye@smile.fr>
2022-09-22 15:23:08 +0200

    feat: add catalog context and provider

--- front/example-app/src/components/App/App.tsx
+++ front/example-app/src/components/App/App.tsx
@@ -5,14 +5,10 @@ import Homepage from '../../pages/Homepage/Homepage'
 
 import Header from '../Header/Header'
 import Menu from '../Menu/Menu'
-import AppProvider from '../AppProvider/AppProvider'
+import AppProvider from '../Providers/AppProvider/AppProvider'
 
 function App(): JSX.Element {
   const [menuOpen, setMenuOpen] = useState(false)
-  const [catalogId, setCatalogId] = useState<string | number>('')
-  const [localizedCatalogId, setLocalizedCatalogId] = useState<string | number>(
-    ''
-  )
 
   function handleMenuToggle(): void {
     setMenuOpen((prevState) => !prevState)
@@ -21,13 +17,7 @@ function App(): JSX.Element {
   return (
     <BrowserRouter basename="/example">
       <AppProvider>
-        <Header
-          catalogId={catalogId}
-          localizedCatalogId={localizedCatalogId}
-          onCatalogIdChange={setCatalogId}
-          onLocalizedCatalogIdChange={setLocalizedCatalogId}
-          onMenuToggle={handleMenuToggle}
-        />
+        <Header onMenuToggle={handleMenuToggle} />
         <Menu menuOpen={menuOpen} onMenuToggle={handleMenuToggle} />
         <Routes>
           <Route path="/" element={<Homepage />} />
