151ec41e3510d2c6653218b9e29633c984a79d22
Ponefar <adrienwolf1@gmail.com>
2022-09-23 11:41:57 +0200

    ESPP_445 update : add arrow tooltip

--- pwa/components/atoms/modals/Tooltip.tsx
+++ pwa/components/atoms/modals/Tooltip.tsx
@@ -3,7 +3,7 @@ import { Tooltip as MuiTooltip, TooltipProps } from '@mui/material'
 function Tooltip(props: TooltipProps): JSX.Element {
   const { className, ...tooltipProps } = props
 
-  return <MuiTooltip {...tooltipProps} classes={{ popper: className }} />
+  return <MuiTooltip {...tooltipProps} arrow classes={{ popper: className }} />
 }
 
 export default Tooltip
