77ba6d3c35812001d7d06a4920cb245af94f9efe
gaulet <marvin.gaulet.pro@gmail.com>
2022-10-05 02:43:23 +0200

    ESPP_294 : add default category in facets page

--- front/pwa/components/molecules/layout/TitleBlock/TitleBlock.tsx
+++ front/pwa/components/molecules/layout/TitleBlock/TitleBlock.tsx
@@ -1,15 +1,36 @@
 import { styled } from '@mui/system'
-import { ReactNode } from 'react'
+import { CSSProperties, ReactNode } from 'react'
 
 import { getCustomScrollBarStyles } from 'shared'
 
-const CustomTitle = styled('div')(({ theme }) => ({
+const CustomTitleNoBorder = styled('div')(({ theme }) => ({
   color: theme.palette.colors.neutral[900],
   fontWeight: 600,
   fontSize: '14px',
   lineHeight: '20px',
   fontFamily: 'Inter',
   padding: theme.spacing(2),
+  paddingBottom: 0,
+}))
+
+const CustomTitleBorder = styled(CustomTitleNoBorder)(({ theme }) => ({
+  paddingBottom: theme.spacing(2),
+  borderBottom: '1px solid',
+  borderColor: theme.palette.colors.neutral[300],
+}))
+
+const CustomSubTitleNoBorder = styled('div')(({ theme }) => ({
+  color: theme.palette.colors.neutral[600],
+  fontWeight: 400,
+  fontSize: '12px',
+  lineHeight: '18px',
+  fontFamily: 'Inter',
+  padding: theme.spacing(2),
+  paddingBottom: 0,
+}))
+
+const CustomSubTitleBorder = styled(CustomSubTitleNoBorder)(({ theme }) => ({
+  paddingBottom: theme.spacing(2),
   borderBottom: '1px solid',
   borderColor: theme.palette.colors.neutral[300],
 }))
@@ -31,16 +52,29 @@ const PaddingBox = styled('div')(({ theme }) => ({
 
 interface IProps {
   title: string
-  children: ReactNode
+  children?: ReactNode
+  style?: CSSProperties
+  borderBottom?: boolean
+  hasSubTitle?: boolean
 }
 
-function TitleBlock({ title, children }: IProps): JSX.Element {
+function TitleBlock(props: IProps): JSX.Element {
+  const { title, children, style, borderBottom = true, hasSubTitle } = props
+  const CustomTitle = borderBottom
+    ? hasSubTitle
+      ? CustomSubTitleBorder
+      : CustomTitleBorder
+    : hasSubTitle
+    ? CustomSubTitleNoBorder
+    : CustomTitleNoBorder
   return (
     <>
-      <CustomTitle>{title}</CustomTitle>
-      <Container>
-        <PaddingBox>{children}</PaddingBox>
-      </Container>
+      <CustomTitle style={style}>{title}</CustomTitle>
+      {Boolean(children) && (
+        <Container>
+          <PaddingBox>{children}</PaddingBox>
+        </Container>
+      )}
     </>
   )
 }
