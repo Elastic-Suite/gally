c06c20ba19a0a366f336f58a718c3aef1786dfb8
Botis <botis@smile.fr>
2022-10-07 17:05:00 +0200

    ESPP-443 On cache-control header add 'must-revalidate' value to disable browser cache

---
+++ api/src/Elasticsuite/Standard/src/Cache/EventSubscriber/ResponseSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Cache\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * On cache-control header add 'must-revalidate' value to disable browser cache.
+ */
+class ResponseSubscriber implements EventSubscriberInterface
+{
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->addCacheControlDirective('must-revalidate');
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}
