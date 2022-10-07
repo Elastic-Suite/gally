b528c19544c4a1156ba5cda3daa814991df613f9
Ponefar <adrienwolf1@gmail.com>
2022-10-07 17:05:01 +0200

    ESPP-fix-indicators-dashboard

--- front/pwa/pages/admin/dashboard.tsx
+++ front/pwa/pages/admin/dashboard.tsx
@@ -1,6 +1,17 @@
 import { withAuth } from '~/hocs'
+import React, { useContext, useEffect } from 'react'
+import { breadcrumbContext } from '~/contexts'
+import { useRouter } from 'next/router'
 
 function Dashboard(): JSX.Element {
+  const router = useRouter()
+
+  const [, setBreadcrumb] = useContext(breadcrumbContext)
+
+  useEffect(() => {
+    setBreadcrumb(['dashboard'])
+  }, [router.query, setBreadcrumb])
+
   // todo: temporary page
   return <div>Dashboard</div>
 }
