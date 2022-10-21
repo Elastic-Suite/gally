2782b4a34fad49010be5302bd05f4f31c253234c
Tony CABAYE <tony.cabaye@smile.fr>
2022-10-21 11:57:46 +0200

    feat: display operators from endpoint

--- front/shared/src/constants/rules.ts
+++ front/shared/src/constants/rules.ts
@@ -1,7 +1,6 @@
 import {
   IRuleAttribute,
   IRuleCombination,
-  RuleAttributeOperator,
   RuleAttributeType,
   RuleCombinationOperator,
   RuleType,
@@ -21,71 +20,3 @@ export const emptyAttributeRule: IRuleAttribute = {
   attribute_type: RuleAttributeType.TEXT,
   value: '',
 }
-
-export const operatorsByType = new Map([
-  [RuleAttributeType.BOOLEAN, [RuleAttributeOperator.IS]],
-  [
-    RuleAttributeType.CATEGORY,
-    [RuleAttributeOperator.IS_ONE_OF, RuleAttributeOperator.IS_NOT_ONE_OF],
-  ],
-  [
-    RuleAttributeType.DROPDOWN,
-    [RuleAttributeOperator.IS_ONE_OF, RuleAttributeOperator.IS_NOT_ONE_OF],
-  ],
-  [
-    RuleAttributeType.FLOAT,
-    [
-      RuleAttributeOperator.EQ,
-      RuleAttributeOperator.NEQ,
-      RuleAttributeOperator.GT,
-      RuleAttributeOperator.GTE,
-      RuleAttributeOperator.LT,
-      RuleAttributeOperator.LTE,
-    ],
-  ],
-  [
-    RuleAttributeType.INT,
-    [
-      RuleAttributeOperator.EQ,
-      RuleAttributeOperator.NEQ,
-      RuleAttributeOperator.GT,
-      RuleAttributeOperator.GTE,
-      RuleAttributeOperator.LT,
-      RuleAttributeOperator.LTE,
-    ],
-  ],
-  [
-    RuleAttributeType.NUMBER,
-    [
-      RuleAttributeOperator.EQ,
-      RuleAttributeOperator.NEQ,
-      RuleAttributeOperator.GT,
-      RuleAttributeOperator.GTE,
-      RuleAttributeOperator.LT,
-      RuleAttributeOperator.LTE,
-    ],
-  ],
-  [
-    RuleAttributeType.PRICE,
-    [
-      RuleAttributeOperator.EQ,
-      RuleAttributeOperator.NEQ,
-      RuleAttributeOperator.GT,
-      RuleAttributeOperator.GTE,
-      RuleAttributeOperator.LT,
-      RuleAttributeOperator.LTE,
-    ],
-  ],
-  [
-    RuleAttributeType.REFERENCE,
-    [RuleAttributeOperator.CONTAINS, RuleAttributeOperator.DOES_NOT_CONTAINS],
-  ],
-  [
-    RuleAttributeType.SELECT,
-    [RuleAttributeOperator.IS_ONE_OF, RuleAttributeOperator.IS_NOT_ONE_OF],
-  ],
-  [
-    RuleAttributeType.TEXT,
-    [RuleAttributeOperator.CONTAINS, RuleAttributeOperator.DOES_NOT_CONTAINS],
-  ],
-])
