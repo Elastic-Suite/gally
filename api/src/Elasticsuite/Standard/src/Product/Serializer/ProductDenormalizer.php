a88e63d64bac43fcc4fff150074987153b706753
Richard BAYET <richard.bayet@smile.fr>
2022-10-23 18:33:11 +0200

    ESPP-273 Product (+de)normalization refactoring

--- api/src/Elasticsuite/Standard/src/Product/Serializer/ProductDenormalizer.php
+++ api/src/Elasticsuite/Standard/src/Product/Serializer/ProductDenormalizer.php
@@ -16,13 +16,11 @@ declare(strict_types=1);
 
 namespace Elasticsuite\Product\Serializer;
 
-use Elasticsuite\Entity\Decoration\GraphQl\Type\StitchingFieldsBuilder;
 use Elasticsuite\Entity\Model\Attribute\StructuredAttributeInterface;
 use Elasticsuite\Entity\Model\Attribute\Type\NestedAttribute;
-use Elasticsuite\Metadata\Model\SourceField;
-use Elasticsuite\Metadata\Repository\MetadataRepository;
 use Elasticsuite\Product\Model\Product;
 use Elasticsuite\Search\Model\Document;
+use Elasticsuite\Stitching\Service\SerializerService;
 use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
@@ -33,8 +31,9 @@ class ProductDenormalizer implements ContextAwareDenormalizerInterface, Denormal
 
     private const ALREADY_CALLED_DENORMALIZER = 'ProductDenormalizerCalled';
 
-    public function __construct(private MetadataRepository $metadataRepository, )
-    {
+    public function __construct(
+        private SerializerService $serializerService
+    ) {
     }
 
     /**
@@ -66,51 +65,28 @@ class ProductDenormalizer implements ContextAwareDenormalizerInterface, Denormal
 
             $contextAttributes = array_diff_key($context['attributes'] ?? [], array_fill_keys(Product::DEFAULT_ATTRIBUTES, true));
             if (!empty($contextAttributes) && isset($data['_source'])) {
-                if ($productMetadata = $this->metadataRepository->findOneBy(['entity' => 'product'])) {
-                    /*
-                     * TODO : either a hard global (all source fields) cache existing between calls
-                     *        OR a hard per-request (context attributes only) cache existing between calls
-                     */
-                    $sourceFieldsTypes = [];
-                    /** @var SourceField $sourceField */
-                    foreach ($productMetadata->getSourceFields() as $sourceField) {
-                        $sourceFieldCode = $sourceField->getCode();
-                        if (!\in_array($sourceField->getType(), array_keys(StitchingFieldsBuilder::STITCHING_ATTRIBUTE_CLASS_TYPE), true)) {
-                            continue;
-                        }
-                        if ($sourceField->isNested()) {
-                            [$path, $field] = [$sourceField->getNestedPath(), $sourceField->getNestedCode()];
-                            $sourceFieldsTypes[$path][$field] = StitchingFieldsBuilder::STITCHING_ATTRIBUTE_CLASS_TYPE[$sourceField->getType()];
-                        } else {
-                            $sourceFieldsTypes[$sourceFieldCode] = StitchingFieldsBuilder::STITCHING_ATTRIBUTE_CLASS_TYPE[$sourceField->getType()];
-                        }
-                    }
+                $sourceFieldsTypes = $this->serializerService->getStitchingConfigFromContextAttributes(
+                    'product',
+                    $contextAttributes
+                );
 
-                    // Looping on context|attributes instead to only parse requested attributes/fields.
-                    foreach ($contextAttributes as $attributeCode => $subStructure) {
-                        if (\array_key_exists($attributeCode, $sourceFieldsTypes) && \array_key_exists($attributeCode, $data['_source'])) {
-                            $attributeType = $sourceFieldsTypes[$attributeCode];
-                            $attributeValue = $data['_source'][$attributeCode];
-                            if (\is_array($subStructure)) {
-                                if (\is_array($sourceFieldsTypes[$attributeCode])) {
-                                    // Individual nested fields.
-                                    $subStructureKeys = array_keys($subStructure);
-                                    $product->addAttribute(
-                                        new NestedAttribute($attributeCode, $attributeValue, $subStructureKeys)
-                                    );
-                                } elseif (is_subclass_of($attributeType, StructuredAttributeInterface::class)) {
-                                    // Structured/Complex fields, value is transmitted as is.
-                                    $product->addAttribute(
-                                        new $attributeType($attributeCode, $attributeValue) // @phpstan-ignore-line
-                                    );
-                                } else {
-                                    if (\is_array($attributeValue)) {
-                                        $attributeValue = json_encode($attributeValue);
-                                    }
-                                    $product->addAttribute(
-                                        new $attributeType($attributeCode, $attributeValue)
-                                    );
-                                }
+                // Looping on context|attributes instead to only parse requested attributes/fields.
+                foreach ($contextAttributes as $attributeCode => $subStructure) {
+                    if (\array_key_exists($attributeCode, $sourceFieldsTypes) && \array_key_exists($attributeCode, $data['_source'])) {
+                        $attributeType = $sourceFieldsTypes[$attributeCode];
+                        $attributeValue = $data['_source'][$attributeCode];
+                        if (\is_array($subStructure)) {
+                            if (\is_array($sourceFieldsTypes[$attributeCode])) {
+                                // Individual nested fields.
+                                $subStructureKeys = array_keys($subStructure);
+                                $product->addAttribute(
+                                    new NestedAttribute($attributeCode, $attributeValue, $subStructureKeys)
+                                );
+                            } elseif (is_subclass_of($attributeType, StructuredAttributeInterface::class)) {
+                                // Structured/Complex fields, value is transmitted as is.
+                                $product->addAttribute(
+                                    new $attributeType($attributeCode, $attributeValue) // @phpstan-ignore-line
+                                );
                             } else {
                                 if (\is_array($attributeValue)) {
                                     $attributeValue = json_encode($attributeValue);
@@ -119,6 +95,10 @@ class ProductDenormalizer implements ContextAwareDenormalizerInterface, Denormal
                                     new $attributeType($attributeCode, $attributeValue)
                                 );
                             }
+                        } else {
+                            $product->addAttribute(
+                                new $attributeType($attributeCode, $attributeValue)
+                            );
                         }
                     }
                 }
