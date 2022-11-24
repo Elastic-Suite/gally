f7fe49d3c226e1dad6904df458ee8b8eeb43e4f5
Ponefar <adrienwolf1@gmail.com>
2022-11-24 15:15:18 +0100

    ESPP_264 fix : bug

--- front/pwa/components/molecules/layout/user/User.test.tsx
+++ front/pwa/components/molecules/layout/user/User.test.tsx
@@ -3,8 +3,17 @@ import { renderWithProviders } from '~/utils/tests'
 import User from './User'
 
 describe('User', () => {
-  it('match snapshot', () => {
-    const { container } = renderWithProviders(<User />)
+  it('match snapshot not connected', () => {
+    const { container } = renderWithProviders(
+      <User isConnectedWithValidJwt={false} />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('match snapshot connected', () => {
+    const { container } = renderWithProviders(
+      <User isConnectedWithValidJwt />
+    )
     expect(container).toMatchSnapshot()
   })
 })
