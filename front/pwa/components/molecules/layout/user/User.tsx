0393841a403f3f093d0bf68f0ee77e4854bea6fe
Ponefar <adrienwolf1@gmail.com>
2022-11-24 14:13:05 +0100

    ESPP_554 logout

--- front/pwa/components/molecules/layout/user/User.tsx
+++ front/pwa/components/molecules/layout/user/User.tsx
@@ -1,6 +1,9 @@
 import Help from '~/components/atoms/help/Help'
 import UserMenu from '~/components/atoms/userMenu/UserMenu'
 import { styled } from '@mui/system'
+import { useContext } from 'react'
+import { userContext } from '~/contexts'
+import { isValidUser } from '~/../shared'
 
 const CustomRoot = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -9,7 +12,11 @@ const CustomRoot = styled('div')(({ theme }) => ({
   marginLeft: 'auto',
 }))
 
-function User(): JSX.Element {
+function User(): JSX.Element | null {
+  const user = useContext(userContext)
+  const isConnected = isValidUser(user)
+
+  if (!isConnected) return
   return (
     <CustomRoot>
       <Help />
