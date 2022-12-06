7d99ff66cdf5dba5403d9a9856f256d817c21045
Ponefar <adrienwolf1@gmail.com>
2022-12-06 17:25:46 +0100

    ESPP_561

--- front/pwa/components/stateful-pages/SettingsAttributes/SettingsAttributes.tsx
+++ front/pwa/components/stateful-pages/SettingsAttributes/SettingsAttributes.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
 import { ITabContentProps, ITableConfig, ITableRow } from 'shared'
+import Alert from '~/components/atoms/Alert/Alert'
 import { useFilters, useResource } from '~/hooks'
+
+import { useTranslation } from 'react-i18next'
 
 import ResourceTable from '../ResourceTable/ResourceTable'
 
@@ -7,6 +11,12 @@ const fixedFilters = { 'metadata.entity': 'product' }
 
 function SettingsAttributes(props: ITabContentProps): JSX.Element {
   const { active } = props
+  const [
+    isVisibleAlertSettingsAttributes,
+    setIsVisibleAlertSettingsAttributes,
+  ] = useState(true)
+
+  const { t } = useTranslation('settings')
 
   const resource = useResource('SourceField')
   const [activeFilters, setActiveFilters] = useFilters(resource)
@@ -16,14 +26,23 @@ function SettingsAttributes(props: ITabContentProps): JSX.Element {
   }
 
   return (
-    <ResourceTable
-      active={active}
-      activeFilters={activeFilters}
-      filters={fixedFilters}
-      getTableConfigs={getTableConfigs}
-      resourceName="SourceField"
-      setActiveFilters={setActiveFilters}
-    />
+    <>
+      {Boolean(isVisibleAlertSettingsAttributes) && (
+        <Alert
+          message={t('settingsAttributes.alert')}
+          onShut={(): void => setIsVisibleAlertSettingsAttributes(false)}
+          style={{ marginBottom: '16px' }}
+        />
+      )}
+      <ResourceTable
+        active={active}
+        activeFilters={activeFilters}
+        filters={fixedFilters}
+        getTableConfigs={getTableConfigs}
+        resourceName="SourceField"
+        setActiveFilters={setActiveFilters}
+      />
+    </>
   )
 }
 
