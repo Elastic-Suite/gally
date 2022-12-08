84289446cba45fe6f37843b4ac83cb73a4fcfb9f
Tony CABAYE <tony.cabaye@smile.fr>
2022-12-08 17:35:15 +0100

    test: add unit tests

---
+++ front/example-app/src/hooks/useProductSort.test.ts
@@ -0,0 +1,39 @@
+import { waitFor } from '@testing-library/react'
+
+import { renderHookWithProviders } from '../utils/tests'
+
+import { useProductSort } from './useProductSort'
+
+jest.mock('./useGraphql', () => ({
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  useGraphqlApi: () => [
+    {
+      data: {
+        categorySortingOptions: [
+          { label: 'Position', code: 'category__position' },
+          { label: 'Name', code: 'name' },
+          { label: 'Final price', code: 'price__price' },
+          { label: 'Stock status', code: 'stock__status' },
+          { label: 'Relevance', code: '_score' },
+        ],
+      },
+    },
+    jest.fn(),
+    jest.fn(),
+  ],
+}))
+
+describe('useProductSort', () => {
+  it('should load the sortOptions', async () => {
+    const { result } = renderHookWithProviders(() => useProductSort())
+    await waitFor(() =>
+      expect(result.current[2]).toEqual([
+        { label: 'Position', value: 'category__position' },
+        { label: 'Name', value: 'name' },
+        { label: 'Final price', value: 'price__price' },
+        { label: 'Stock status', value: 'stock__status' },
+        { label: 'Relevance', value: '_score' },
+      ])
+    )
+  })
+})
