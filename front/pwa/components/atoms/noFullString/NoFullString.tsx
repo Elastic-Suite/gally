77219227df3d4c6fe5d2e49109f1159c1073009d
Ponefar <adrienwolf1@gmail.com>
2022-12-14 02:09:17 +0100

    ESPP_566 delete name from userMenu and create new atoms : no full string

---
+++ front/pwa/components/atoms/noFullString/NoFullString.tsx
@@ -0,0 +1,29 @@
+import { firstLetterUppercase } from '~/../shared'
+import InfoTooltip from '../form/InfoTooltip'
+
+interface IProps {
+  name: string
+  max?: number
+  toolTip?: boolean
+  firstLetterUpp?: boolean
+}
+
+function NoFullString(props: IProps): JSX.Element {
+  const { name, max = 28, toolTip, firstLetterUpp } = props
+  let newName = ''
+  if (name.length > max) {
+    newName = `${name.substring(0, max)}...`
+    return toolTip ? (
+      <InfoTooltip title={name} noStyle>
+        {firstLetterUpp ? firstLetterUppercase(newName) : newName}
+      </InfoTooltip>
+    ) : firstLetterUpp ? (
+      <>{firstLetterUppercase(newName)}</>
+    ) : (
+      <>{newName}</>
+    )
+  }
+  return <>{firstLetterUpp ? firstLetterUppercase(name) : name}</>
+}
+
+export default NoFullString
