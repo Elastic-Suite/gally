5b27174eefdc7bb1afa199172748113685879ae1
Tony CABAYE <tony.cabaye@smile.fr>
2023-01-05 13:46:18 +0100

    fix: to make it work in dev

--- front/docker-entrypoint-pwa.sh
+++ front/docker-entrypoint-pwa.sh
@@ -4,4 +4,15 @@ fixuid || :
 
 yarn install --frozen-lockfile
 
+if [ -d gally-admin ]
+then
+  cd gally-admin
+  yarn install --frozen-lockfile;
+  yarn build
+  cd ../pwa/node_modules
+  ln -s ../../gally-admin/packages/components gally-admin-components
+  ln -s ../../gally-admin/packages/shared gally-admin-shared
+  cd ../..
+fi
+
 exec $@
