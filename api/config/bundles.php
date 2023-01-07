56b432b18727d5115cbe02fd0b9a4b90b1592df2
Romain Ruaud <romain.ruaud@smile.fr>
2023-01-07 10:44:02 +0100

    ESPP-628 Use gally package and empty src.

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -19,6 +19,4 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-
-    Gally\VirtualCategory\GallyVirtualCategoryBundle::class => ['all' => true],
 ];
--- api/src/Gally/Standard/docs/index.md
+++
@@ -1,2 +0,0 @@
-@Todo file useful ? see https://symfony.com/doc/current/bundles/best_practices.html#documentation
-The root file for the Bundle documentation. (a link link to documentation ?)
