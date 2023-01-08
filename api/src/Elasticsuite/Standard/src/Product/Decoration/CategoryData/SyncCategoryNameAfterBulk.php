5a2cd0decabececbf0c0bcc0ee1b827c4282bf0c
Richard BAYET <richard.bayet@smile.fr>
2023-01-08 15:28:24 +0100

    ESPP-553 Post-bulk update of products category name for search

---
+++ api/src/Elasticsuite/Standard/src/Product/Decoration/CategoryData/SyncCategoryNameAfterBulk.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @package   Elasticsuite
+ * @author    ElasticSuite Team <elasticsuite@smile.fr>
+ * @copyright 2022 Smile
+ * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
+ *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ */
+
+declare(strict_types=1);
+
+namespace Elasticsuite\Product\Decoration\CategoryData;
+
+use ApiPlatform\Core\GraphQl\Resolver\MutationResolverInterface;
+use Elasticsuite\Index\Model\Index;
+use Elasticsuite\Product\Service\CategoryNameUpdater;
+
+class SyncCategoryNameAfterBulk implements MutationResolverInterface
+{
+    public function __construct(
+        private MutationResolverInterface $decorated,
+        private CategoryNameUpdater $categoryNameUpdater
+    ) {
+    }
+
+    /**
+     * @param object|null $item
+     *
+     * @return object|null
+     */
+    public function __invoke($item, array $context)
+    {
+        /** @var Index $index */
+        $index = $this->decorated->__invoke($item, $context);
+
+        if (null !== $index->getEntityType()) {
+            if ('category' === $index->getEntityType()) {
+                // Handle category name change ?
+            }
+
+            if (('product' === $index->getEntityType()) && $index->getLocalizedCatalog()) {
+                // Handle copying category.name to category._name
+                $this->categoryNameUpdater->updateCategoryNames(
+                    $index,
+                    json_decode($context['args']['input']['data'], true)
+                );
+            }
+        }
+
+        return $index;
+    }
+}
