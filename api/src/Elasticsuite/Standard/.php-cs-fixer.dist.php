6cfb005cf1a7bc007b2407f281066f1c26258fff
Richard BAYET <richard.bayet@smile.fr>
2023-01-10 14:07:03 +0100

    ESPP-613 Fix header license file

--- api/src/Elasticsuite/Standard/.php-cs-fixer.dist.php
+++ api/src/Elasticsuite/Standard/.php-cs-fixer.dist.php
@@ -11,9 +11,8 @@ versions in the future.
 
 @package   Elasticsuite
 @author    ElasticSuite Team <elasticsuite@smile.fr>
-@copyright 2022 Smile
-@license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
-           Unauthorized copying of this file, via any medium, is strictly prohibited.
+@copyright 2022-present Smile
+@license   Open Software License v. 3.0 (OSL-3.0)
 HEADER;
 
 $finder = PhpCsFixer\Finder::create()
