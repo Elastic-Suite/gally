323a09854a4b99aa0e0b902b1667482f803b892c
Botis <botis@smile.fr>
2023-01-11 12:49:22 +0100

    ESPP-624 Remove condition to run migrations in php/docker-entrypoint.sh

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -48,9 +48,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			echo "The database is now ready and reachable"
 		fi
 
-		if ls -A migrations/*.php >/dev/null 2>&1; then
-			php bin/console doctrine:migrations:migrate --no-interaction
-		fi
+		php bin/console doctrine:migrations:migrate --no-interaction
 	fi
 fi
 
