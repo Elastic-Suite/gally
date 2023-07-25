1e5e515268fcc19bfc100c0c86f55171c76418bf
Botis <botis@smile.fr>
2023-07-25 17:37:11 +0200

    Set initial scale value

--- front/pwa/pages/_app.tsx
+++ front/pwa/pages/_app.tsx
@@ -26,6 +26,7 @@ function App(props: AppProps): JSX.Element {
     <>
       <Head>
         <title>Gally Admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <main className={inter.variable}>
         <GallyApp {...props} />
