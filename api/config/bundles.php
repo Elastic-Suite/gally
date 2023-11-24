3dd6ea7862d6abaf69c3054f638016c58c654e9d
Pierre Gauthier <pigau@smile.fr>
2023-11-24 13:50:15 +0100

    [vectorSearch] Add gally Premium

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -20,4 +20,8 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Gally\VirtualCategory\GallyVirtualCategoryBundle::class => ['all' => true],
+    Gally\Boost\GallyBoostBundle::class => ['all' => true],
+    Gally\Thesaurus\GallyThesaurusBundle::class => ['all' => true],
+    Gally\VectorSearch\GallyVectorSearchBundle::class => ['all' => true],
 ];
