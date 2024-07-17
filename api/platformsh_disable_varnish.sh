b907f48bc96c215f346c2973b4519d557bd59900
Pierre Gauthier <pigau@smile.fr>
2024-07-17 09:15:47 +0200

    [Docker] #1303328 - Fix install prod mode

--- api/platformsh_disable_varnish.sh
+++ api/platformsh_disable_varnish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Purpose of this script is to disable the Varnish integration of API-Platform. There is no Varnish service in the deployment configuration ATM.
-sed -e 'H;x;/^\(  *\)\n\1/{s/\n.*//;x;d;}' -i 's/.*//;x;/http_cache/{s/^\( *\).*/ \1/;x;d;}' config/packages/api_platform.yaml
-sed -e 'H;x;/^\(  *\)\n\1/{s/\n.*//;x;d;}' -i 's/.*//;x;/http_cache/{s/^\( *\).*/ \1/;x;d;}' vendor/gally/gally-standard/src/Configuration/Resources/config/api_platform.yaml
+#sed -e 'H;x;/^\(  *\)\n\1/{s/\n.*//;x;d;}' -i 's/.*//;x;/http_cache/{s/^\( *\).*/ \1/;x;d;}' config/packages/api_platform.yaml
+#sed -e 'H;x;/^\(  *\)\n\1/{s/\n.*//;x;d;}' -i 's/.*//;x;/http_cache/{s/^\( *\).*/ \1/;x;d;}' vendor/gally/gally-standard/src/Configuration/Resources/config/api_platform.yaml
