ef2f2f3da8e600e68b81a6555f9d4db037cd81b0
Matthias GOUPIL <matthiasgoup@gmail.com>
2024-07-26 15:17:14 +0200

    feat: correction retour

--- front/example-app/src/services/product.ts
+++ front/example-app/src/services/product.ts
@@ -22,96 +22,103 @@ const durationFormat: Record<DurationKey, string> = {
 export function getProductFilters(
   activeFilters: IActiveFilters
 ): IProductFieldFilterInput[] {
-  const data: IProductFieldFilterInput[] = []
-  activeFilters.forEach((activeFilter) => {
-    const precAcc = data.find(
-      (value) => Object.keys(value)[0] === activeFilter.filter.field
-    )
-    const acc: IProductFieldFilterInput = precAcc || {}
-    if (activeFilter.filter.type === AggregationType.CATEGORY) {
-      acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
-        eq: activeFilter.value,
-      } as ICategoryTypeDefaultFilterInputType
-    } else if (activeFilter.filter.type === AggregationType.BOOLEAN) {
-      acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
-        eq: true,
-      } as IStockTypeDefaultFilterInputType
-    } else if (activeFilter.filter.type === AggregationType.SLIDER) {
-      acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
-        lte: Number(activeFilter.value),
-      } as IEntityIntegerTypeFilterInput
-    } else if (activeFilter.filter.type === AggregationType.CHECKBOX) {
-      if (!(activeFilter.filter.field in acc)) {
+  return activeFilters.reduce<IProductFieldFilterInput[]>(
+    (data, activeFilter) => {
+      const field = activeFilter.filter.field as keyof IProductFieldFilterInput
+      let acc = data.find((value) => Object.keys(value)[0] === field)
+
+      if (!acc) {
+        acc = {} as IProductFieldFilterInput
+        data.push(acc)
+      }
+
+      if (activeFilter.filter.type === AggregationType.CATEGORY) {
         acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
-          in: [],
+          eq: activeFilter.value,
+        } as ICategoryTypeDefaultFilterInputType
+      } else if (activeFilter.filter.type === AggregationType.BOOLEAN) {
+        acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
+          eq: true,
+        } as IStockTypeDefaultFilterInputType
+      } else if (activeFilter.filter.type === AggregationType.SLIDER) {
+        acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
+          lte: Number(activeFilter.value),
+        } as IEntityIntegerTypeFilterInput
+      } else if (activeFilter.filter.type === AggregationType.CHECKBOX) {
+        if (!(activeFilter.filter.field in acc)) {
+          acc[activeFilter.filter.field as keyof IProductFieldFilterInput] = {
+            in: [],
+          }
         }
-      }
-      ;(
-        acc[
-          activeFilter.filter.field as keyof IProductFieldFilterInput
-        ] as ISelectTypeDefaultFilterInputType
-      ).in.push(activeFilter.value)
-    } else if (activeFilter.filter.type === AggregationType.HISTOGRAM_DATE) {
-      if (!acc.boolFilter?._should) {
-        acc.boolFilter = { _should: [] }
-      }
-      const field = activeFilter.filter.field as keyof IProductFieldFilterInput
-      const date = parse(
-        activeFilter.value,
-        activeFilter.filter.date_format,
-        new Date()
-      )
-
-      const incrementString = activeFilter.filter.date_range_interval
-      const incrementNumber = Number(
-        incrementString.substring(0, incrementString.length - 1)
-      )
-      const incrementType = incrementString.substring(
-        incrementString.length - 1
-      ) as DurationKey
-
-      const gt = format(date, activeFilter.filter.date_format)
-      const lt = format(
-        add(date, {
-          [durationFormat[incrementType]]: incrementNumber,
-        }),
-        activeFilter.filter.date_format
-      )
-
-      acc.boolFilter._should.push({
-        [field]: {
-          gt,
-          lt,
-        },
-      })
-    } else if (activeFilter.filter.type === AggregationType.HISTOGRAM) {
-      if (!acc.boolFilter?._should) {
-        acc.boolFilter = { _should: [] }
-      }
-      const field = activeFilter.filter.field as keyof IProductFieldFilterInput
-      const arrayValue = activeFilter.value.split('-')
-      const [firstValue, secondValue] = arrayValue
-
-      if (arrayValue.length > 1) {
-        const isFirstValueIsAsterisk = firstValue === '*'
-        const isSecondValueIsAsterisk = secondValue === '*'
-        let data: Record<string, number> = {}
-
-        if (isFirstValueIsAsterisk && !isSecondValueIsAsterisk) {
-          data = { lt: Number(secondValue) }
-        } else if (!isFirstValueIsAsterisk && !isSecondValueIsAsterisk) {
-          data = { gte: Number(firstValue), lte: Number(secondValue) }
-        } else {
-          data = { gt: Number(firstValue) }
+        ;(
+          acc[
+            activeFilter.filter.field as keyof IProductFieldFilterInput
+          ] as ISelectTypeDefaultFilterInputType
+        ).in.push(activeFilter.value)
+      } else if (activeFilter.filter.type === AggregationType.HISTOGRAM_DATE) {
+        if (!acc.boolFilter?._should) {
+          acc.boolFilter = { _should: [] }
         }
+        const field = activeFilter.filter
+          .field as keyof IProductFieldFilterInput
+        const date = parse(
+          activeFilter.value,
+          activeFilter.filter.date_format,
+          new Date()
+        )
+
+        const incrementString = activeFilter.filter.date_range_interval
+        const incrementNumber = Number(
+          incrementString.substring(0, incrementString.length - 1)
+        )
+        const incrementType = incrementString.substring(
+          incrementString.length - 1
+        ) as DurationKey
+
+        const gt = format(date, activeFilter.filter.date_format)
+        const lt = format(
+          add(date, {
+            [durationFormat[incrementType]]: incrementNumber,
+          }),
+          activeFilter.filter.date_format
+        )
 
         acc.boolFilter._should.push({
-          [field]: data,
+          [field]: {
+            gt,
+            lt,
+          },
         })
-      }
-    }
-    if (!precAcc) data.push(acc)
-  })
+      } else if (activeFilter.filter.type === AggregationType.HISTOGRAM) {
+        if (!acc.boolFilter?._should) {
+          acc.boolFilter = { _should: [] }
+        }
+        const field = activeFilter.filter
+          .field as keyof IProductFieldFilterInput
+        const arrayValue = activeFilter.value.split('-')
+        const [firstValue, secondValue] = arrayValue
 
-  return data
+        if (arrayValue.length > 1) {
+          const isFirstValueIsAsterisk = firstValue === '*'
+          const isSecondValueIsAsterisk = secondValue === '*'
+          let data: Record<string, number> = {}
+
+          if (isFirstValueIsAsterisk && !isSecondValueIsAsterisk) {
+            data = { lt: Number(secondValue) }
+          } else if (!isFirstValueIsAsterisk && !isSecondValueIsAsterisk) {
+            data = { gte: Number(firstValue), lte: Number(secondValue) }
+          } else {
+            data = { gt: Number(firstValue) }
+          }
+
+          acc.boolFilter._should.push({
+            [field]: data,
+          })
+        }
+      }
+
+      return data
+    },
+    []
+  )
 }
