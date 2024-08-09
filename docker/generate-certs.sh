c1f8cb5eb2d93faa996df70ec30737308f4a85b6
Pierre Gauthier <pigau@smile.fr>
2024-08-09 18:09:26 +0200

    Fix generate certs script

--- docker/generate-certs.sh
+++ docker/generate-certs.sh
@@ -10,4 +10,4 @@ mkdir -p docker/certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout docker/certs/server.key \
   -out docker/certs/server.crt \
-  -subj "/CN=${PWA_SERVER_NAME-localhost}"
+  -subj "/CN=${PWA_SERVER_NAME:-localhost}"
