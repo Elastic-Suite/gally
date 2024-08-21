7fed913f5b9d030ce31ab45c4c656bc2f9dca0b0
Pierre Gauthier <pigau@smile.fr>
2024-08-21 16:21:10 +0200

    [Docker] Backport new config

--- api/tests/object-manager.php
+++ api/tests/object-manager.php
@@ -1,11 +1,12 @@
 <?php
 
-use Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
