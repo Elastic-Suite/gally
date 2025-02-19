b81839530afd4602f7219aa9d22c38f6ba88aeda
Romain Ruaud <romain.ruaud@smile.fr>
2025-02-19 21:15:18 +0100

    Adding yarn service for PWA and various improvements

--- docker/mono-container/docker-entrypoint.sh
+++ docker/mono-container/docker-entrypoint.sh
@@ -4,12 +4,23 @@
 
 sleep 10
 
-# Todo move this in dockerfile in order to have everything ready in the built iamge
+# Todo move this in dockerfile in order to have everything ready in the built image
 cd /var/gally/api
+
+echo "Entering Gally build configuration"
+
 bin/console lexik:jwt:generate-keypair --skip-if-exists
 bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
-bin/console gally:vector-search:upload-model
+
+PACKAGE="gally/gally-premium"
+if composer show "$PACKAGE" > /dev/null 2>&1; then
+	echo "Gally Premium is installed, prepare Vector Search."
+    bin/console gally:vector-search:upload-model
+fi
+
 bin/console hautelook:fixture:load
+
+echo "Gally Application is ready..."
 
 tail -f --retry --follow=name -n0 \
 	/var/gally/api/var/log/dev.log
