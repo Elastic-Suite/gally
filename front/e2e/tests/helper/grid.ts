936a40e5c5cc923e31e113dc3edac94242ba6ee2
Matthias GOUPIL <matthiasgoup@gmail.com>
2025-05-21 16:40:55 +0200

    feat(#1330810): add helper for the Grid component

---
+++ front/e2e/tests/helper/grid.ts
@@ -0,0 +1,60 @@
+import { Locator, Page, expect } from '@playwright/test'
+import { Pagination } from './pagination'
+
+export class Grid {
+  private page: Page
+  private gridDataTestId: string
+
+  private grid: Locator
+  public pagination: Pagination
+
+  constructor(page: Page, gridDataTestId: string) {
+    this.page = page
+    this.gridDataTestId = gridDataTestId
+    this.pagination = new Pagination(page, `${gridDataTestId}Pagination`)
+  }
+
+  private async getGrid(): Promise<Locator> {
+    if (!this.grid) {
+      this.grid = await this.page.getByTestId(this.gridDataTestId)
+    }
+    return await this.grid
+  }
+
+  public async expectHeadersToBe(headerNames: string[]): Promise<void> {
+    const grid = await this.getGrid()
+    await this.page.waitForSelector(
+      `[data-testId="${this.gridDataTestId}"] thead tr > th`,
+      { state: 'attached' }
+    )
+
+    await expect(
+      await grid.locator('thead tr > th').filter({ hasText: /\S/})
+    ).toHaveText(headerNames, {
+      useInnerText: true,
+    })
+  }
+
+  public async getCountLines(): Promise<number> {
+    return await this.pagination.getCount()
+  }
+
+  public async expectToFindLineWhere(
+    columnName: string,
+    value: string
+  ): Promise<void> {
+    const grid = await this.getGrid()
+    const columnHeaders = await grid.locator('thead tr th').allInnerTexts()
+    const columnIndex = columnHeaders.findIndex(
+      (header) => header.trim() === columnName
+    )
+
+    if (columnIndex === -1) {
+      throw new Error(`Column named "${columnName}" not found`)
+    }
+
+    expect(
+      await grid.locator(`tbody tr td:nth-child(${columnIndex + 1})`)
+    ).toContainText([value])
+  }
+}
