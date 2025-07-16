32e9ce7bde7997b7b10eabd27b6a4f53b71b71e5
kerwan <aurelienperonnet@gmail.com>
2025-07-16 11:36:46 +0200

    feat(#1349827):  Run tests as multiple roles

--- front/e2e/tests/helper/auth.ts
+++ front/e2e/tests/helper/auth.ts
@@ -1,6 +1,42 @@
-import { Page, expect } from '@playwright/test'
+import { Page, expect, test } from '@playwright/test'
 
-export async function login(page: Page) {
+interface IUserCredentials {
+  email: `${string}@${string}.${Lowercase<string>}`
+  password: string
+}
+
+const rolesInfos: Record<UserRole, IUserCredentials> = {
+  admin: {
+    email: 'admin@example.com',
+    password: 'apassword',
+  },
+  contributor: {
+    email: 'contributor@example.com',
+    password: 'apassword',
+  },
+}
+
+export enum UserRole {
+  ADMIN = 'admin',
+  CONTRIBUTOR = 'contributor',
+}
+
+export function runTestsAsRoles(
+  roles: UserRole[],
+  callback: (page: Page, role?: UserRole) => Promise<void>
+): void {
+  for (const role of roles) {
+    // Instantiate an isolated test context for each role
+    test(`Test as ${role} role`, async ({ page }) => {
+      await login(page, role)
+      // Run callback with a page authenticated with the specified role
+      await callback(page, role)
+    })
+  }
+}
+
+
+export async function login(page: Page, role: UserRole = UserRole.ADMIN): Promise<void> {
   await page.goto('/fr/login')
 
   // Get inputs and submit button
@@ -9,8 +45,8 @@ export async function login(page: Page) {
   const submitButton = await page.getByTestId('submitButton')
 
   // Fill with correct credentials and submit the form
-  await emailInput.fill('admin@example.com')
-  await passwordInput.fill('apassword')
+  await emailInput.fill(rolesInfos[role].email)
+  await passwordInput.fill(rolesInfos[role].password)
   await submitButton.click()
 
   await expect(page).toHaveURL('/fr/admin/settings/scope/catalogs', {})
