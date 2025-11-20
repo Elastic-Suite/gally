ad28bc81b6cb3d7519411ba21a2f4637a4dfd9d3
kerwan <aurelienperonnet@gmail.com>
2025-11-20 14:44:40 +0100

    [Docker] Improve pwa entrypoint comments

--- docker/front/docker-entrypoint-pwa.sh
+++ docker/front/docker-entrypoint-pwa.sh
@@ -2,12 +2,12 @@
 
 fixuid || :
 
-# Skip node_modules deletion and yarn install if SKIP_INSTALL is set to true
+# Skip node_modules deletion and yarn install if SKIP_YARN_INSTALL is set to true
 # This is disabled by default as clean install are often necessary
 # Change this in env file at your own risk with the benefit of speeding up container startup
 if [ "$SKIP_YARN_INSTALL" != "true" ]; then
     echo "=== STARTING NODE_MODULES DELETION AND YARN INSTALL ==="
-    echo "SKIP_YARN_INSTALL is not set to true"
+    echo "SKIP_YARN_INSTALL is set to false or is not defined"
 
   rm -rf node_modules pwa/node_modules example-app/node_modules
 
@@ -27,7 +27,7 @@ if [ "$SKIP_YARN_INSTALL" != "true" ]; then
   fi
 else
   echo "=== SKIPPING NODE_MODULES DELETION AND YARN INSTALL ==="
-  echo "SKIP_INSTALL is set to true"
+  echo "SKIP_YARN_INSTALL is set to true"
 fi
 
 exec $@
