2e066ceadb052086094347a3e4a8950e8fb413a5
Pierre Gauthier <pigau@smile.fr>
2026-02-04 14:57:10 +0100

    Update to Api Platfrom 4.2

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -19,7 +19,7 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Gally\GallyBundle::class => ['all' => true],
     CoopTilleuls\ForgotPasswordBundle\CoopTilleulsForgotPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Gally\GallyBundle::class => ['all' => true],
 ];
